722c7ae7b36b9653443d77fdd2d95eeab29607f0
Yousef <yeshaq@realtermenergy.com>
2021-06-15 20:48:13 -0400

    added footer and header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome To Shop Pop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
