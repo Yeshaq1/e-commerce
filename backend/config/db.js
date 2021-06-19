d909e1005a4b1b81500c3baf05f623d6da1121a2
Yousef <yeshaq@realtermenergy.com>
2021-06-18 21:20:53 -0400

    added data models, connected MongoDb

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    console.log(
+      `MongoDB is connected ... ${connect.connection.host}`.cyan.underline
+    );
+  } catch (err) {
+    console.error(`Error: ${err.message}`.red.underline.bold);
+    //exit process by force if error.
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import products from './data/products.js';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+//connect DB (Mongo)
+connectDB();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('API is running...');
@@ -16,4 +25,9 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log('Server running on port 5000'));
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
